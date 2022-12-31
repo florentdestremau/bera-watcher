43b40b3c866baf1f6f0260826220d9b85f7b23b8
Florent Destremau <florent.destremau@gmail.com>
2022-12-31 10:59:16 +0100

    Added email sending for BERA

--- src/EventSubscriber/BeraSubscriber.php
+++ src/EventSubscriber/BeraSubscriber.php
@@ -3,10 +3,18 @@
 namespace App\EventSubscriber;
 
 use App\Event\BeraCreatedEvent;
+use App\Repository\SubscriberRepository;
+use App\Service\SendBeraByEmailService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class BeraSubscriber implements EventSubscriberInterface
 {
+    public function __construct(
+        private SubscriberRepository $subscriberRepository,
+        private SendBeraByEmailService $sendBeraByEmailService
+    ) {
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -19,7 +27,11 @@ class BeraSubscriber implements EventSubscriberInterface
         $bera = $event->bera;
 
         if ($bera->getDate()->format('Y-m-d') === date('Y-m-d')) {
-            // Todo: send notification
+            $subscribers = $this->subscriberRepository->findBy(['mountains' => $bera->getMountain()]);
+
+            foreach ($subscribers as $subscriber) {
+                $this->sendBeraByEmailService->sendEmail($bera, $subscriber);
+            }
         }
     }
 }
