8fd543206fd2eda9488b2a51c8175c5ff2fdf569
Florent Destremau <florent.destremau@gmail.com>
2023-01-18 22:42:01 +0100

    Updated email display and added image

--- src/Notifier/OnBeraExtractNotification.php
+++ src/Notifier/OnBeraExtractNotification.php
@@ -7,6 +7,7 @@ use App\Entity\Subscriber;
 use Symfony\Bridge\Twig\Mime\NotificationEmail;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Part\DataPart;
 use Symfony\Component\Notifier\Message\EmailMessage;
 use Symfony\Component\Notifier\Notification\EmailNotificationInterface;
 use Symfony\Component\Notifier\Notification\Notification;
@@ -17,36 +18,44 @@ class OnBeraExtractNotification extends Notification implements EmailNotificatio
 {
     public function __construct(private Bera $bera, array $channels = [])
     {
-        parent::__construct("BERA pour {$this->bera->getMountain()->value} le {$this->bera->getDate()->format('d/m/Y')}", $channels);
+        parent::__construct("", $channels);
     }
 
     public function asEmailMessage(EmailRecipientInterface|Subscriber $recipient, string $transport = null): ?EmailMessage
     {
         $crawler = new Crawler($this->bera->getXml());
+        $risk = $crawler->filterXPath('//RISQUE')->extract(['RISQUEMAXI'])[0];
+        $comment = $crawler->filterXPath('//RISQUE')->extract(['COMMENTAIRE'])[0];
+        $summary = $crawler->filterXPath('//RESUME')->text('', false);
+        $stability = $crawler->filterXPath('//STABILITE/TEXTE')->text('', false);
+        $quality = $crawler->filterXPath('//QUALITE/TEXTE')->text('', false);
 
         $email = (new NotificationEmail())
             ->markAsPublic()
             ->to(new Address($recipient->getEmail()))
-            ->subject($this->getSubject())
+            ->subject("BERA: risque $risk pour {$this->bera->getMountain()->value} le {$this->bera->getDate()->format('d/m/Y')}")
+            ->addPart((new DataPart(base64_decode($crawler->filterXPath('//ImageCartoucheRisque/Content')->text()), 'risque.png'))->asInline())
             ->markdown(
                 <<<EOT
 Bonjour,
 
-Un nouveau BERA est disponible pour le massif **{$this->bera->getMountain()->value}**.
+Un nouveau BERA est disponible pour le massif **{$this->bera->getMountain()->value}**. 
 
-![BERA](data:image/png;base64,{$crawler->filterXPath('//ImageCartoucheRisque/Content')->text()})
+Risque maximal: {$risk}.
+
+![$comment](cid:risque.png)
 
 **Résumé:**
 
-{$crawler->filterXPath('//RESUME')->text()}
+{$summary}
 
 **Stabilité:**
 
-{$crawler->filterXPath('//STABILITE/TEXTE')->text()}
+{$stability}
 
-**Qualité:**
+**Neige:**
 
-{$crawler->filterXPath('//QUALITE/TEXTE')->text()}
+{$quality}
 
 
 *Vous pouvez éditez vos préférences [ici]({$recipient->getEditLink()}).*
