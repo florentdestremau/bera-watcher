dac4f38e47856a93f7ba5f34957f140a888dbd9f
Florent Destremau <florent.destremau@gmail.com>
2023-01-18 12:33:41 +0100

    Removed local testing code

--- migrations/Version20230118075107.php
+++ migrations/Version20230118075107.php
@@ -35,7 +35,6 @@ final class Version20230118075107 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE bera DROP xml');
     }
 
